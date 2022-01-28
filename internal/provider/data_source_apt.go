9a02bdbc1c363ad6a83933abbb3a0eb97e6a3fbb
Shi Han NG <shihanng@gmail.com>
2022-01-28 09:21:36 +0900

    Refactor common codes into dataSourceInstalled

--- internal/provider/data_source_apt.go
+++ internal/provider/data_source_apt.go
@@ -15,7 +15,7 @@ var errNotFound = errors.New("could not find path with suffix")
 
 func dataSourceApt() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: dataSourceAptRead,
+		ReadContext: dataSourceInstalled("dpkg", "-L"),
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
@@ -29,42 +29,46 @@ func dataSourceApt() *schema.Resource {
 	}
 }
 
-func dataSourceAptRead(ctx context.Context, data *schema.ResourceData, _ interface{}) diag.Diagnostics { //nolint:dupl
-	var diags diag.Diagnostics
+func dataSourceInstalled(args ...string) schema.ReadContextFunc {
+	return func(ctx context.Context, data *schema.ResourceData, _ interface{}) diag.Diagnostics {
+		var diags diag.Diagnostics
 
-	name := data.Get("name").(string) // nolint:forcetypeassert
+		name := data.Get("name").(string) // nolint:forcetypeassert
 
-	cmd := exec.CommandContext(ctx, "dpkg", "-L", name)
+		args = append(args, name)
 
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return diag.Diagnostics{
-			diag.Diagnostic{
-				Severity: diag.Error,
-				Summary:  fmt.Sprintf("'%s' returns %v", strings.Join(cmd.Args, " "), err.Error()),
-				Detail:   string(out),
-			},
+		cmd := exec.CommandContext(ctx, args[0], args[1:]...) // nolint:gosec
+
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return diag.Diagnostics{
+				diag.Diagnostic{
+					Severity: diag.Error,
+					Summary:  fmt.Sprintf("'%s' returns %v", strings.Join(cmd.Args, " "), err.Error()),
+					Detail:   string(out),
+				},
+			}
 		}
+
+		paths := strings.Split(string(out), "\n")
+
+		validPath, err := findPathHasSuffix(paths, "bin/"+name)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		if err := data.Set("name", name); err != nil {
+			return diag.FromErr(err)
+		}
+
+		if err := data.Set("path", validPath); err != nil {
+			return diag.FromErr(err)
+		}
+
+		data.SetId(validPath)
+
+		return diags
 	}
-
-	paths := strings.Split(string(out), "\n")
-
-	validPath, err := findPathHasSuffix(paths, "bin/"+name)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	if err := data.Set("name", name); err != nil {
-		return diag.FromErr(err)
-	}
-
-	if err := data.Set("path", validPath); err != nil {
-		return diag.FromErr(err)
-	}
-
-	data.SetId(validPath)
-
-	return diags
 }
 
 func findPathHasSuffix(paths []string, suffix string) (string, error) {
--- internal/provider/data_source_brew.go
+++ internal/provider/data_source_brew.go
@@ -1,18 +1,12 @@
 package provider
 
 import (
-	"context"
-	"fmt"
-	"os/exec"
-	"strings"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func dataSourceBrew() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: dataSourceBrewRead,
+		ReadContext: dataSourceInstalled("brew", "list"),
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
@@ -24,42 +18,4 @@ func dataSourceBrew() *schema.Resource {
 			},
 		},
 	}
-}
-
-func dataSourceBrewRead(ctx context.Context, data *schema.ResourceData, _ interface{}) diag.Diagnostics { //nolint:dupl
-	var diags diag.Diagnostics
-
-	name := data.Get("name").(string) // nolint:forcetypeassert
-
-	cmd := exec.CommandContext(ctx, "brew", "list", name)
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return diag.Diagnostics{
-			diag.Diagnostic{
-				Severity: diag.Error,
-				Summary:  fmt.Sprintf("'%s' returns %v", strings.Join(cmd.Args, " "), err.Error()),
-				Detail:   string(out),
-			},
-		}
-	}
-
-	paths := strings.Split(string(out), "\n")
-
-	validPath, err := findPathHasSuffix(paths, "bin/"+name)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	if err := data.Set("name", name); err != nil {
-		return diag.FromErr(err)
-	}
-
-	if err := data.Set("path", validPath); err != nil {
-		return diag.FromErr(err)
-	}
-
-	data.SetId(validPath)
-
-	return diags
 }
