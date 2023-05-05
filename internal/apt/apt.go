b7a92ca49f80a885e7796d059f511518175ac382
Shi Han NG <shihanng@gmail.com>
2023-05-06 08:44:37 +0900

    apt: Handle name=version notation as apt name

--- internal/apt/apt.go
+++ internal/apt/apt.go
@@ -22,7 +22,9 @@ func Install(ctx context.Context, name string) error {
 }
 
 func FindInstalled(ctx context.Context, name string) (string, error) {
-	cmd := exec.CommandContext(ctx, "dpkg", "-L", name)
+	info := GetInfo(name)
+
+	cmd := exec.CommandContext(ctx, "dpkg", "-L", info.Name) //nolint:gosec
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -31,6 +33,20 @@ func FindInstalled(ctx context.Context, name string) (string, error) {
 		}
 
 		return "", errors.Wrap(errors.WithDetail(err, string(out)), strings.Join(cmd.Args, " "))
+	}
+
+	if info.Version != "" {
+		cmd := exec.CommandContext(ctx, "dpkg", "-s", info.Name) //nolint:gosec
+
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", errors.Wrap(errors.WithDetail(err, string(out)), strings.Join(cmd.Args, " "))
+		}
+
+		installedVersion := ExtractVersion(string(out))
+		if info.Version != installedVersion {
+			return "", xerrors.ErrNotInstalled
+		}
 	}
 
 	paths := strings.Split(string(out), "\n")
@@ -47,4 +63,39 @@ func Uninstall(ctx context.Context, name string) error {
 	}
 
 	return nil
+}
+
+// Info contains the package name and the package version.
+type Info struct {
+	Name    string
+	Version string
+}
+
+const nPart = 2
+
+// GetInfo splits the name and version from string name=version and put the
+// values into Info.
+func GetInfo(original string) Info {
+	var info Info
+
+	splitted := strings.SplitN(original, "=", nPart)
+
+	info.Name = splitted[0]
+
+	if len(splitted) == nPart {
+		info.Version = splitted[1]
+	}
+
+	return info
+}
+
+// ExtractVersion extracts version value from the output of dpkg -s <package>.
+func ExtractVersion(input string) string {
+	for _, line := range strings.Split(input, "\n") {
+		if strings.HasPrefix(line, "Version: ") {
+			return strings.TrimSpace(strings.TrimPrefix(line, "Version: "))
+		}
+	}
+
+	return ""
 }
